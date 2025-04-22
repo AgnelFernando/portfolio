15d4699291e98a0675e7944b070f0fa9bbe12ad1
gilesagnel <gilesagnel@gmail.com>
2025-04-21 22:49:47 -0500

    fixed timeline colors

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -18,14 +18,14 @@
           <div class="flex w-10p justify-center z-10 ml-1">
             <div class="p-1.5 bg-accent2 rounded-full border-border border-m h-1 w-1 self-center" />
           </div>
-          <div :class="['text-sm text-button-text self-center w-45p', isExp(event) ? 'text-left' : 'text-right']">
+          <div :class="['text-sm text-text-title self-center w-45p', isExp(event) ? 'text-left' : 'text-right']">
             {{ event.date }}
           </div>
         </div>
       </transition-group>
     </div>
     <div class="flex justify-center mt-2">
-      <button @click="expanded = !expanded" class="flex py-3 text-button-text flex items-center hover:button-hover transition">
+      <button @click="expanded = !expanded" class="flex py-3 text-text-subtitle flex items-center hover:text-text-hover transition">
         <span class="transition-opacity duration-300">{{ expanded ? 'View Less' : 'View More' }}</span>
         <svg v-if="!expanded" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-double-down" viewBox="0 0 16 16">
           <path fill-rule="evenodd" d="M1.646 6.646a.5.5 0 0 1 .708 0L8 12.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708"/>
