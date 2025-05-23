007a3555159c595b424418cbc6b14f7957e646f5
gilesagnel <gilesagnel@gmail.com>
2025-05-23 14:12:19 -0500

    Add Paper Review in blogs

--- src/components/Blogs.vue
+++ src/components/Blogs.vue
@@ -3,12 +3,12 @@
         <Title titlename="Blogs" />
         <div class="flex flex-wrap justify-around container">
             <div v-for="(blog, index) in blogs" :key="index" @click="openBlog(blog.link)" :class="[
-                'm-3 rounded-lg border overflow-hidden bg-card-background border-border shadow-lg w-96 flex flex-col h-full transition-transform duration-200 ease-in-out cursor-default',
+                'm-3 rounded-lg border overflow-hidden bg-card-background border-border shadow-lg w-1/3 h-full flex flex-col transition-transform duration-200 ease-in-out cursor-default',
                 blog.link ? 'transform hover:shadow-2xl hover:scale-105' : '']">
 
                 <!-- blog Image -->
-                <div class="w-full relative">
-                    <img :src="blog.image" alt="blog Image" class="w-full h-full object-cover">
+                <div class="w-full h-64 relative">
+                    <img :src="blog.image" alt="blog Image" class="w-full h-full object-inherent">
                     <div class="absolute inset-0 bg-gradient-to-tr from-transparent via-transparent to-overlay"></div>
                 </div>
 
@@ -49,7 +49,20 @@ export default {
                     image: require('../assets/images/blogs/openvla/header.jpg'),
                     description: 'Lessons learnt from Controlling a UR3 Robot with Open VLA',
                     link: '#/blog/openvla'
+                },
+                {
+                    name: 'Walk these Ways - Paper Review',
+                    image: require('../assets/images/blogs/wtw/header.png'),
+                    description: 'Walk These Ways: Tuning Robot Control for Generalization with Multiplicity of Behavior',
+                    link: 'https://youtu.be/36g-YFETCiM'
+                },
+                {
+                    name: 'AlphaFold2 - Paper Review',
+                    image: require('../assets/images/blogs/alphafold/header.webp'),
+                    description: 'Highly accurate protein structure prediction with AlphaFold',
+                    link: 'https://youtu.be/KiiiVMqGcKA'
                 }
+
             ]
         };
     }
